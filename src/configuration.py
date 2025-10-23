1e907a6f71d2abb6ba5c982729f7b15e82892ec6
DzMS <dzigmihail@gmail.com>
2025-10-23 20:50:38 +0300

    DEMO-QA-003 Implement getting selenium driver

--- src/configuration.py
+++ src/configuration.py
@@ -2,6 +2,10 @@ import configparser
 import os
 from pathlib import Path
 
+from selenium.webdriver.chrome.webdriver import WebDriver as ChromeDriver
+from selenium.webdriver.firefox.webdriver import WebDriver as FirefoxDriver
+from selenium.webdriver.remote.webdriver import WebDriver as RemoteDriver
+
 
 class Configuration:
 
@@ -12,12 +16,28 @@ class Configuration:
 
     def get_browser(self):
         """Get selenium instance of driver (chrome, firefox etc.)"""
-        # TODO DEMO-QA-003 Implement getting selenium driver in config class
-        pass
+        browser = self._get_driver()
+        wait_time = self._parser.getfloat('browser', 'wait')
+        browser.implicitly_wait(wait_time)
+        maximize = self._parser.getboolean('browser', 'maximize')
+        if maximize:
+            browser.maximize_window()
+
+        return browser
+
+    @staticmethod
+    def _get_driver():
+        driver = os.getenv("BROWSER", "Chrome")
+        if driver == "Chrome":
+            return ChromeDriver()
+        elif driver == "Firefox":
+            return FirefoxDriver()
+        else:
+            return RemoteDriver()
 
     def get_base_url(self):
-        return self._parser['demoqa'].get('url')
-
+        # return self._parser['demoqa'].get('url')
+        return self._parser.get('demoqa', 'url')
 
 
 CONFIG = Configuration()
